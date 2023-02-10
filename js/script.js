ebeee11b8dfe48a775b0221489d1e293d3edfeb3
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-10 23:29:25 +0100

    JS code for "endGame" functions

--- js/script.js
+++ js/script.js
@@ -47,6 +47,13 @@
 
 // SOLUZIONE CON CREAZIONE DELLE CELLE TRAMITE LA CREAZIONE DI UN ELEMENTO DEL DOM
 
+// VARIABILI GLOBALI
+let bombe = [];
+let punteggio;
+let punteggioMax;
+let latoGriglia;
+
+
 // 1. Recuperare elemento Pulsante Play da DOM
 const playBtnEl = document.querySelector('button');
 
@@ -76,9 +83,11 @@ function starGame(){
 
   // 1. Invoco la Funzione "resetGame" per iniziare ogni volta una nuova partita da capo.
   resetGame();
+
+  // 2. Imposto valore lato della griglia
+  latoGriglia = 10;
   
-  // 2. Invoco la Funzione "generaGriglia" passando la dimensione del lato [Gliglia Quadrata 10x10]  
-  const latoGriglia = 10;
+  // 4. Invoco la Funzione "generaGriglia" passando la dimensione del lato [Gliglia Quadrata]  
   generaGriglia(latoGriglia);
   
 }
@@ -113,6 +122,35 @@ function generaBombe(posizioneMax){
 
 }
 
+// ******************************
+// FUNZIONE "generaNumeriCaselle"
+// *****************************
+
+function generaNumeriCelle(numMax){
+
+  // 1. Creo Array in cui salvare la i numeri delle celle;
+  let numeriCelle = [];
+
+  // 2. Creo variabile per memorizzare Numero Random
+  let numeroRandom;
+
+  // 3. Creo variabile per contare quandi elemento ho inserito nell'Array
+  let count = 0;
+
+  // 4. Creo un ciclo che mi genera 100 numeri rando tutti diversi
+  while(count!==numMax){
+
+    numeroRandom = Math.floor(Math.random() * (numMax) + 1);
+    if (numeriCelle.includes(numeroRandom)=== false){
+      numeriCelle.push(numeroRandom);
+      count++;
+    }
+
+  }
+  
+  return numeriCelle;
+}
+
 // *************************
 // FUNZIONE "generaGriglia"
 // *************************
@@ -122,12 +160,17 @@ function generaGriglia(lato){
   // 1. Calcolo numero celle con il parametro della funzione
   const numCelle = lato * lato;
 
-  // 2. Invoco la Funzione "generaBombe" passando il "numero celle" come parametro per avere le posizioni delle Bombe
-  const bombe = generaBombe(numCelle);
+  // 2. Invoco la Funzione "generaBombe", passando il "numero celle" come parametro, per avere le posizioni delle Bombe
+  bombe = generaBombe(numCelle);
   console.log(bombe);
 
-  // 3. Creo variabile che tiene conto del Punteggio
-  let punteggio = 0;
+  // 3. Invoco la Funzione "generaNumeriCelle", passando il "numero celle" come parametro, per avere le posizioni delle Celle
+  const posizioni = generaNumeriCelle(numCelle);
+  console.log(posizioni);
+
+  // 4. Inizializzo variabile che tiene conto del Punteggio e calcola il Punteggio Massimo
+  punteggio = 0;
+  punteggioMax = numCelle - bombe.length;
 
 
   // 3. Creo Ciclo di 'numCelle' interazione ed a ogni interazione creo una Cella da inserire dentro 'GrigliaEl'
@@ -140,37 +183,10 @@ function generaGriglia(lato){
     cellaEl.style.cssText += `width:calc(100% / ${lato})`;
 
     // 3.3 - Aggiungo contenuto (numero della cella) alla Cella
-    cellaEl.innerHTML = i + 1;
+    cellaEl.innerHTML = posizioni[i];
 
     // 4. Aggancio evento "click" sulla cella e gestisco il gioco
-    cellaEl.addEventListener('click', function(){
-
-      const cella = this;
-
-      // 4.1 Controllo se il Numero della Cella corrisponde a una Bomba ---> Si = SCONFITTA , No = VITTORIA
-      if (bombe.includes(parseInt(cella.innerHTML))){
-
-        // Cambio colore di sfondo
-        cella.classList.add('bg-sconfitta');
-
-        // Inserisco icona "bomba"
-        cella.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
-
-
-      } else {
-
-        // Cambio colore di sfondo
-        cella.classList.add('bg-vittoria');
-
-        // Inserisco icona "ok"
-        cella.innerHTML = `<i class="fa-solid fa-thumbs-up"></i>`;
-
-        // Incremento punteggio
-        punteggio++;
-        console.log(punteggio);
-      }
-
-    });
+    cellaEl.addEventListener('click', clickCella);
 
     // 5. Inserisco la Cella creata dentro la Griglia nel DOM
     grigliaEl.append(cellaEl);
@@ -182,13 +198,80 @@ function generaGriglia(lato){
 // FUNZIONE "clickCella"
 // *************************
 
-// function clickCella(){
+function clickCella(){
 
-//   const cellaEl = this;
+  // 1. Tramite 'this' recupero la cella a cui e' agganciato l'evento "click"
+  const cella = this;
 
-//   console.log(cellaEl.innerHTML);
-//   console.log(bombe);
+  // 2. Recupero elemento del DOM in cui inserire il punteggio
+  const punteggioEl = document.querySelector('.punteggio');
 
-//   cellaEl.classList.add('bg-vittoria');
+  // 3. Controllo se il Numero della Cella corrisponde a una Bomba ---> Si = SCONFITTA , No = VITTORIA
+   if (bombe.includes(parseInt(cella.innerHTML))){
+
+    // Cambio colore di sfondo
+    cella.classList.add('bg-sconfitta');
+    
+    // Inserisco icona "bomba"
+    cella.innerHTML = `<i style="color:black" class="fa-solid fa-bomb"></i>`;
+
+    // Richiamo funzione per determinale la Sconfitta
+    endGameLose(punteggio);
+
+  } else {
+
+    cella.removeEventListener('click', clickCella);
+
+    // Cambio colore di sfondo
+    cella.classList.add('bg-vittoria');
+
+    // Inserisco icona "ok"
+    cella.innerHTML = `<i style="color:black" class="fa-solid fa-thumbs-up"></i>`;
+
+    // Incremento punteggio
+    punteggio++;
+    punteggioEl.innerHTML = `<h5>Punteggio Attuale: <span style="color:blue">${punteggio}</span></h5>`;
+
+    // Richiamo funzione per dichiare la Vittoria
+    endGameVictory(punteggio,punteggioMax);
+  }
   
-// }
+}
+
+// *************************
+// FUNZIONE "endGame"
+// *************************
+
+function endGameLose(punti){
+
+  // 1. Messaggio di Sconfitta con Punteggio
+  alert ('Hai perso :( \n' + `Il tuo punteggio finale e' ${punti}`);
+
+  // 2. Ricarico la Pagina per poter iniziare una Nuova Partita.
+  window.location.reload();
+
+}
+
+// *************************
+// FUNZIONE "endGameVictory"
+// *************************
+
+function endGameVictory(punti,puntiMax){
+
+  // 1. Controlla per determinare la vittoria
+  if (punti === puntiMax){
+    alert ("Complimenti! \n Hai vinto!");
+
+    window.location.reload();
+  }
+}
+
+// *************************
+// FUNZIONE "endGameError"
+// *************************
+
+function endGameError(){
+  alert(`Seleziona una Difficolta' ;)`);
+
+  window.location.reload();
+}
